822f30176003c86b7b9fc82b280ea3fd31fd9bf0
Alan Young <harleypig@harleypig.com>
2025-10-12 21:53:30 +0000

    added gollum config

---
+++ config.rb
@@ -0,0 +1,10 @@
+wiki_options = {
+  critic_markup: true,
+  emoji: true,
+  page_file_dir: 'pages',
+  index_page: 'home',
+  sidebar: :left,
+}
+
+# Load wiki options into the proper space
+Precious::App.set(:wiki_options, wiki_options)
